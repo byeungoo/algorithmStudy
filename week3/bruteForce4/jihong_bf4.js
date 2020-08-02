b73d2adf446414035ea6762200c3c9a981ef7dec
Gred <jihong.park.dev@gmail.com>
2020-08-02 12:55:30 +0900

    [지홍파] 3주차 - 완전탐색 카펫

---
+++ week3/bruteForce4/jihong_bf4.js
@@ -0,0 +1,19 @@
+function solution(brown, yellow) {
+  let bWidth = brown / 2 - 1;
+  let bHeight = 3;
+  const answer = [];
+  while (bWidth >= bHeight) {
+    if (yellow == (bHeight - 2) * (bWidth - 2)) {
+      console.log(bWidth, bHeight);
+      answer.push(bWidth);
+      answer.push(bHeight);
+    }
+
+    bWidth -= 1;
+    bHeight += 1;
+  }
+
+  return answer;
+}
+
+console.log(solution(10, 2));
