3e89a9178ff3b734db84180fc89f8b423c8abc08
heesu <tedlee0314@gmail.com>
2020-08-22 23:30:59 +0900

    희수파 dp2
    .

---
+++ week6/dp2/heesu_dp_2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = -999;
+        int[][] dp = new int[triangle.length][triangle.length];
+
+        dp[0][0] = triangle[0][0];
+        for(int i=1;i<triangle.length;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0){
+                    dp[i][j] = dp[i-1][j] + triangle[i][j];
+                }else if(j==i){
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                }
+            }
+        }
+        for(int i=0;i<triangle.length;i++){
+            if(answer< dp[triangle.length-1][i]) answer = dp[triangle.length-1][i];
+        }
+
+        return answer;
+    }
+}
