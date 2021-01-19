5ed41834a6e4f460cfbeb97cd25d04d149498bb4
ByeonGooHoon <dksekfldks65@naver.com>
2021-01-19 17:31:57 +0900

    [구훈파] 경주로 건설

--- 2020 kakao internship/경주로 건설/goohoon_raceway_build.java
+++ 2020 kakao internship/경주로 건설/goohoon_raceway_build.java
@@ -1,79 +1,96 @@
-/*
- * 풀이시간 : 1시간
-   점수 : 82.6 (케이스 4개 시간초과)
-*/ 
-
 import java.util.*;
 
 class Solution {
-
-    static int answer = 2147483647;
+    static int answer = Integer.MAX_VALUE;
     static int size = 0;
     static int[][] next = { {0,1}, {1,0}, {-1,0}, {0, -1} };
 
-    public int solution(int[][] board) {
+    public static int solution(int[][] board) {
         size = board.length;
         int[][] boardArr = new int[size+2][size+2];
         initData(board, boardArr);
-        boardArr[1][1] = 1;
         bfs(boardArr, 1, 1, 0, null);
         return answer;
     }
 
-    private void bfs(int[][] boardArr, int x, int y, int cost, String dir){
+    private static void bfs(int[][] boardArr, int x, int y, int cost, String dir){
 
-        //base case
-        if(x == size && y == size){
-            answer = answer > cost ? cost : answer;
-            return;
-        } else if(cost>answer){
-            return;
-        }
+        Queue<Car> q = new LinkedList<>();
+        Car car = new Car(1, 1, null, 0);
+        boardArr[1][1] = 1;
+        q.add(car);
 
-        for(int i=0;i<4;i++){
-            int nx = x + next[i][0];
-            int ny = y + next[i][1];
-            if(boardArr[nx][ny] == 0){
+        while (!q.isEmpty()){
+
+            Car curCar = q.poll();
+
+            //base case
+            if(curCar.x == size && curCar.y == size){
+                answer = Math.min(curCar.cost, answer);
+                continue;
+            }
+
+            for(int i=0;i<4;i++){
+                int nx = curCar.x + next[i][0];
+                int ny = curCar.y + next[i][1];
+
                 String nextDir = "";
-
-                if(i == 0)
+                if(i == 0 || i == 3)
                     nextDir = "h";
-                else if(i == 1)
+                else if(i == 1 || i == 2)
                     nextDir = "v";
-                else if(i == 2)
-                    nextDir = "v";
-                else if(i == 3)
-                    nextDir = "h";
 
-                boardArr[nx][ny] = 1;
-                bfs(boardArr, nx, ny,  getCost(dir, nextDir, cost), nextDir);
-                boardArr[nx][ny] = 0;
+                int nextCost = curCar.calculateNextCost(nextDir);
+
+                if(boardArr[nx][ny] == 0){
+                    Car nextCar = new Car(nx, ny, nextDir, nextCost );
+                    boardArr[nx][ny] = nextCost;
+                    q.add(nextCar);
+                } else if(boardArr[nx][ny] >= nextCost){
+                    Car nextCar = new Car(nx, ny, nextDir, nextCost);
+                    boardArr[nx][ny] = nextCost;
+                    q.add(nextCar);
+                }
             }
         }
     }
-    private int getCost(String curDir, String nextDir, int cost){
 
-        if(curDir == null){
-            cost += 100;
-        } else if(curDir.equals(nextDir)){
-            cost += 100;
-        } else if(!curDir.equals(nextDir)){
-            cost += 500 + 100;
-        }
-
-        return cost;
-    }
-
-    private void initData(int[][] board, int[][] boardArr) {
+    private static void initData(int[][] board, int[][] boardArr) {
         int len = boardArr.length;
-        for(int i=0;i<len;i++) {
+        for(int i=0;i<len;i++)
             Arrays.fill(boardArr[i], 1);
-        }
-        for(int i=1;i<len-1;i++){
-            for(int j=1;j<boardArr.length-1;j++){
+
+        for(int i=1;i<len-1;i++)
+            for(int j=1;j<boardArr.length-1;j++)
                 boardArr[i][j] = board[i-1][j-1];
-            }
+    }
+
+}
+
+class Car{
+
+    public int x;
+    public int y;
+    public String dir;
+    public int cost;
+
+    Car(int x, int y, String dir, int cost){
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.cost = cost;
+    }
+
+    int calculateNextCost(String nextDir){
+        int cost = this.cost;
+        if(this.dir == null){
+            cost += 100;
+        } else if(this.dir.equals(nextDir)){
+            cost += 100;
+        } else if(!this.dir.equals(nextDir)){
+            cost += 600;
         }
+        return cost;
     }
 
 }
